631ebec04e416c8d6598a0379658d89891423fae
jparangdev <jparangdev@gmail.com>
2022-04-11 14:05:28 +0900

    98p useState 사용하기

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
-import Greeting from './component/Greeting';
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, Button} from 'react-native';
 import Box from './component/Box';
 
 const App = () => {
-  const name = 'JSX';
+  const [visible, setVisible] = useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
   return (
     <SafeAreaView>
-      {/*주석을 작성해 봅시*/}
-      {/*<Greeting name={name} />*/}
-      <Box rounded={true} size="large" color="blue" />
+      <Button title="토글" onPress={onPress} />
+      {visible && <Box rounded={true} size="large" color="blue" />}
     </SafeAreaView>
   );
 };
